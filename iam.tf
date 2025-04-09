ee38e77409907810296b44831c82d2732ca9dde0
unknown <chaudharividya15@gmail.com>
2025-04-09 18:41:18 +0530

    new7

--- iam.tf
+++ iam.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_role" "my_role" {
+resource "aws_iam_role" "my_role1" {
     name = "new_role_for_s3_using_terraform_jenkins_vcvc"
     assume_role_policy = jsonencode({
         Version = "2012-10-17",
